369afdb1aec8da48a0cb681e1dbfd31653070b95
Jon Whittlestone <dev@howapped.com>
2021-10-27 09:26:43 +0100

    initial commit - tdd with jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {label} from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello react/i);
   expect(linkElement).toBeInTheDocument();
+
+  const actual = 1;
+  const expected = 1;
+  expect(actual).toEqual(expected);
+
+});
+
+test('generates a label', () => {
+  const result = label('react');
+  expect(result).toEqual('Hello REACT');
 });
