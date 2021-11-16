7883d64e5e894b7e99201433fa1dda859d80d37a
Jon Whittlestone <jon.whittlestone.fr@harpercollins.co.uk>
2021-11-16 10:58:36 +0000

    class components, passing state down into a functional component

--- book/chapter5-hooks/class-components/src/App.tsx
+++ book/chapter5-hooks/class-components/src/App.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greeting from "./Greeting";
+import Greeting from "./GreetingFunctional";
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      enteredName: "",
+      message: ""
+    }
+    this.onChangeName = this.onChangeName.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Greeting name="Jim Fenton" />
-      </header>
-    </div>
-  );
+  state: { enteredName: string, message: string }
+
+  onChangeName(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      enteredName: e.target.value,
+      message: `Hello from, ${e.target.value}`
+    });
+  }
+
+  render() {
+    console.log('rendering app');
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"          />
+          <input
+            value={this.state.enteredName}
+            onChange={this.onChangeName}
+          ></input>
+          <Greeting message={this.state.message}></Greeting>
+        </header>
+      </div>
+    )
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Greeting name="Jim Fenton" />
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- book/chapter5-hooks/class-components/src/Greeting.tsx
+++ book/chapter5-hooks/class-components/src/Greeting.tsx
@@ -12,14 +12,32 @@ export default class Greeting extends React.Component<GreetingProps, GreetingSta
     constructor(props: GreetingProps) {
         super(props);
         this.state = {
-            message: `Hello from, ${props.name}`
+            message: Greeting.getNewMessage(props.name)
         }
     }
 
+    state: GreetingState;
+
+    /**
+     * This function must always return a state object
+     * @param props 
+     * @param state 
+     * @returns 
+     */
     static getDerivedStateFromProps(props: GreetingProps, state: GreetingState) {
-        console.log(props, state);
+        console.log({ props }, { state });
+        if (props.name && props.name !== state.message) {
+            const newState = { ...state };
+            newState.message = Greeting.getNewMessage(props.name);
+            return newState;
+        }
         return state;
     }
+
+    static getNewMessage(name: string = "") {
+        return `Hello from, ${name}`;
+    }
+
     render() {
         if (!this.props.name) {
             return <div>no name given</div>;
